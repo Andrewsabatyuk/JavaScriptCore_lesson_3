bca5bc6752a02a32c9325d246e3139af68200b26
Andrew <andrewsabatyuk17@gmail.com>
2020-06-17 10:37:23 +0300

    added code examples from lesson-2, first task

---
+++ lesson 2/task 1/script.js
@@ -0,0 +1,8 @@
+let a = prompt('Введіть число');
+if (a > 0){
+    console.log('Число більше 0')
+} else if (a < 0){
+    console.log('число менше 0')
+}else {
+    console.log('Ваше число рівне 0')
+}
